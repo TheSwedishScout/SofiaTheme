28abb586799379dbe5e82dda542196aae180ee29
Max Timje <max.timje@teliacompany.com>
2022-01-06 18:28:57 +0100

    add woocommerse start

--- functions.php
+++ functions.php
@@ -1,21 +1,24 @@
 <?php
-	if ( ! isset( $content_width ) ) $content_width = 1362;
-	include 'functions/admin-function.php';
-	include 'functions/avdelningar.php';
-	include 'functions/avg_lum.php';
-	include 'functions/css_and_js.php';
-	include 'functions/frontpage.php';
-	include 'functions/karnamn.php';
-	include 'functions/menus.php';
-	include 'functions/theme_support.php';
-	include 'functions/widget_areas.php';
-
-	function console_log($output, $with_script_tags = true) {
-    $js_code = 'console.log(' . json_encode($output, JSON_HEX_TAG) . 
-');';
-    if ($with_script_tags) {
-        $js_code = '<script>' . $js_code . '</script>';
+    if (! isset($content_width)) {
+        $content_width = 1362;
     }
-    echo $js_code;
-}
-?>
+    include 'functions/admin-function.php';
+    include 'functions/avdelningar.php';
+    include 'functions/avg_lum.php';
+    include 'functions/css_and_js.php';
+    include 'functions/frontpage.php';
+    include 'functions/karnamn.php';
+    include 'functions/menus.php';
+    include 'functions/theme_support.php';
+    include 'functions/widget_areas.php';
+    include 'functions/woocommerce.php';
+
+    function console_log($output, $with_script_tags = true)
+    {
+        $js_code = 'console.log(' . json_encode($output, JSON_HEX_TAG) .
+');';
+        if ($with_script_tags) {
+            $js_code = '<script>' . $js_code . '</script>';
+        }
+        echo $js_code;
+    }
